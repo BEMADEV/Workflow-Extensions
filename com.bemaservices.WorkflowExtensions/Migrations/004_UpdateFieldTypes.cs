85e91d031300865fd08b2ac09be11a957d8f345d
Taylor Cavaletto <tjcavaletto@gmail.com>
2026-09-25 10:31:31 -0400

    - Migrated ReCaptcha Field Type to Core Captcha Field Type

--- com.bemaservices.WorkflowExtensions/Migrations/004_UpdateFieldTypes.cs
+++ com.bemaservices.WorkflowExtensions/Migrations/004_UpdateFieldTypes.cs
@@ -19,9 +19,22 @@ using Rock.Plugin;
 namespace com.bemaservices.WorkflowExtensions.Migrations
 {
     [MigrationNumber( 4, "1.18.0" )]
-    public class RenameScheduleBuilder : Migration
+    public class UpdateFieldTypes : Migration
     {
         public override void Up()
+        {
+            RenameScheduleBuilder();
+            ReplaceReCaptchaFieldType();
+        }
+
+        public override void Down()
+        {
+            RestoreScheduleBuilderName();
+
+            // The CAPTCHA consolidation cannot be safely reversed without changing CAPTCHA references that originated with Rock core.
+        }
+
+        private void RenameScheduleBuilder()
         {
             RockMigrationHelper.UpdateFieldType(
                 "BEMA Schedule Builder",
@@ -31,7 +44,7 @@ namespace com.bemaservices.WorkflowExtensions.Migrations
                 "9AAF1F39-E485-4CCB-92CF-5F5BA0CD9822" );
         }
 
-        public override void Down()
+        private void RestoreScheduleBuilderName()
         {
             RockMigrationHelper.UpdateFieldType(
                 "Schedule Builder",
@@ -39,6 +52,29 @@ namespace com.bemaservices.WorkflowExtensions.Migrations
                 "com.bemaservices.WorkflowExtensions",
                 "com.bemaservices.WorkflowExtensions.Field.Types.ScheduleBuilderFieldType",
                 "9AAF1F39-E485-4CCB-92CF-5F5BA0CD9822" );
+        }
+
+        private void ReplaceReCaptchaFieldType()
+        {
+            Sql( @"
+        DECLARE @BemaFieldTypeId INT = (Select Top 1 Id From FieldType Where [Class] = 'com.bemaservices.WorkflowExtensions.Field.Types.ReCaptchaFieldType');
+        DECLARE @CoreFieldTypeId INT = (Select Top 1 Id From FieldType Where [Guid] = '22F43337-7177-4064-9D4B-841EAD671678');
+
+        If @BemaFieldTypeId Is Not Null And @CoreFieldTypeId Is Not Null
+        Begin
+        Update EntityType
+        Set SingleValueFieldTypeId = @CoreFieldTypeId
+        Where SingleValueFieldTypeId = @BemaFieldTypeId
+
+        Update EntityType
+        Set MultiValueFieldTypeId = @CoreFieldTypeId
+        Where MultiValueFieldTypeId = @BemaFieldTypeId
+
+        Update Attribute
+        Set FieldTypeId = @CoreFieldTypeId
+        Where FieldTypeId = @BemaFieldTypeId
+        End
+" );
         }
     }
 }
